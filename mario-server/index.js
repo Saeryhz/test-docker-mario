e969164302437248ed61624109f184d29b4e90e0
WilliamRockett <william@rockett.fr>
2023-05-17 11:29:05 +0200

    Image au lieu du Dockerfile

--- mario-server/index.js
+++ mario-server/index.js
@@ -14,25 +14,37 @@ io.on('connect', (socket) => {
 	services.logger.debug('Un joueur s\'est connecté');
 
 	socket.on('getGameData', (callback) => {
-		// On renvoie les données de tous les joueurs du serveur au joueur
-		callback(gameData);
+		try {
+			// On renvoie les données de tous les joueurs du serveur au joueur
+			callback(gameData);
+		} catch (error) {
+
+		}
 	});
 
 	socket.on('onCurrentPlayerConnect', (data) => {
-		// On ajoute le joueur dans gameData
-		let newPlayer = { ...data, playerId: socket.id };
-		gameData.players.push(newPlayer);
-		services.logger.info(`${data.playerName} a rejoint, ${gameData.players.length} joueur(s) en ligne`);
+		try {
+			// On ajoute le joueur dans gameData
+			let newPlayer = { ...data, playerId: socket.id };
+			gameData.players.push(newPlayer);
+			services.logger.info(`${data.playerName} a rejoint, ${gameData.players.length} joueur(s) en ligne`);
 
-		// On prévient tous les joueurs qu'un joueur s'est connecté
-		io.emit('onPlayerConnect', newPlayer);
+			// On prévient tous les joueurs qu'un joueur s'est connecté
+			io.emit('onPlayerConnect', newPlayer);
+		} catch (error) {
+
+		}
 	});
 
 	socket.on('disconnect', () => {
-		let disconnectedPlayer = gameData.players.find(p => p.playerId === socket.id);
-		gameData.players = gameData.players.filter(p => p.playerId !== disconnectedPlayer.playerId);
-		services.logger.info(`${disconnectedPlayer?.playerName} est parti, ${gameData.players.length} joueur(s) en ligne`);
-		io.emit('onPlayerLeave', disconnectedPlayer);
+		try {
+			let disconnectedPlayer = gameData.players.find(p => p.playerId === socket.id);
+			gameData.players = gameData.players.filter(p => p.playerId !== disconnectedPlayer.playerId);
+			services.logger.info(`${disconnectedPlayer?.playerName} est parti, ${gameData.players.length} joueur(s) en ligne`);
+			io.emit('onPlayerLeave', disconnectedPlayer);
+		} catch (error) {
+
+		}
 	});
 
 	socket.on('onPlayerMovement', (data) => {
@@ -48,17 +60,25 @@ io.on('connect', (socket) => {
 	});
 
 	socket.on('onPlayerDeath', (data) => {
-		let player = gameData.players.find(p => p.playerId === socket.id);
-		player.death++;
-		player.isAlive = false; // TODO: ne pas faire spawn les morts
-		services.logger.error(`${player?.playerName} est mort`);
-		io.emit('onPlayerDeath', { deadPlayer: player, killedBy: data.killedBy });
+		try {
+			let player = gameData.players.find(p => p.playerId === socket.id);
+			player.death++;
+			player.isAlive = false; // TODO: ne pas faire spawn les morts
+			services.logger.error(`${player?.playerName} est mort`);
+			io.emit('onPlayerDeath', { deadPlayer: player, killedBy: data.killedBy });
+		} catch (error) {
+
+		}
 	});
 
 	socket.on('onPlayerRespawn', () => {
-		let player = gameData.players.find(p => p.playerId === socket.id);
-		player.isAlive = true;
-		services.logger.info(`${player?.playerName} a respawn`);
-		io.emit('onPlayerRespawn', { player: player });
+		try {
+			let player = gameData.players.find(p => p.playerId === socket.id);
+			player.isAlive = true;
+			services.logger.info(`${player?.playerName} a respawn`);
+			io.emit('onPlayerRespawn', { player: player });
+		} catch (error) {
+
+		}
 	});
 });
